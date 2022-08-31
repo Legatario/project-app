2e6e4a653a710424bd35e758fd3421ce067aed4a
Luis Phillipe <l.phillipe.d@gmail.com>
2022-08-31 19:39:08 -0300

    dashbord

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+// import Button from '@mui/material/Button';import './App.css';
+import Dashboard from './components/Dashboard/dashbord';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div className='App'>
+      <Dashboard/>
     </div>
-  );
+  ); 
 }
+
 
 export default App;
