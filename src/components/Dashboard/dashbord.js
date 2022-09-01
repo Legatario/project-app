b478dfbf26312ae50d70e974bc6ed0fc872bd132
Luis Phillipe <l.phillipe.d@gmail.com>
2022-09-01 16:17:52 -0300

    alert componente creat

--- src/components/Dashboard/dashbord.js
+++ src/components/Dashboard/dashbord.js
@@ -21,6 +21,7 @@ import { mainListItems, secondaryListItems } from './listItems';
 import Chart from './Chart';
 import Deposits from './Deposits';
 import Orders from './Orders';
+import Alert from '../Alert/index';
 
 function Copyright(props) {
   return (
@@ -172,6 +173,7 @@ function DashboardContent() {
                     height: 240,
                   }}
                 >
+                  <Alert type="success">Gráfico atualizado</Alert>
                   <Chart />
                 </Paper>
               </Grid>
