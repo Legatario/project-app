2a63cb7dd5a6ad61c5c193a6505f88c80ad7ed0b
Luis Phillipe <l.phillipe.d@gmail.com>
2022-09-02 16:12:03 -0300

    Updates

---
+++ precache-manifest.1dc63616bea24b0e35677a0efc27367a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a42f5eae45acfe5bd1ef",
+    "url": "/static/css/main.2fe2ef81.chunk.css"
+  },
+  {
+    "revision": "a42f5eae45acfe5bd1ef",
+    "url": "/static/js/main.a42f5eae.chunk.js"
+  },
+  {
+    "revision": "5c9371d4d36d35110571",
+    "url": "/static/js/1.5c9371d4.chunk.js"
+  },
+  {
+    "revision": "5992e1365e94f91885ec",
+    "url": "/static/js/2.5992e136.chunk.js"
+  },
+  {
+    "revision": "14d4912ea3be5e735c87",
+    "url": "/static/js/runtime~main.14d4912e.js"
+  },
+  {
+    "revision": "f606b9ee18f115f63c329d06451a8d0f",
+    "url": "/index.html"
+  }
+];
